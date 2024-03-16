745bd61025e89d5059e43e3b48e1d5d2182945d7
yolk-pie-L <493577282@qq.com>
2024-03-16 17:49:06 +0800

    update output format to base64

--- diffusers/sdxl-turbo.py
+++ diffusers/sdxl-turbo.py
@@ -1,6 +1,8 @@
 from diffusers import AutoPipelineForText2Image
 from kserve import Model, ModelServer
 from typing import Dict, Union
+import base64
+from io import BytesIO
 
 class ImageModel(Model):
     def __init__(self, name: str):
@@ -14,7 +16,10 @@ class ImageModel(Model):
 
     def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
         image = self.pipeline(prompt=payload['instances'][0], num_inference_steps=1, guidance_scale=0.0).images[0]
-        return {"instances": image}
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        return {"instances": img_str}
 
 
 if __name__ == "__main__":
