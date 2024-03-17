bbcba5c07dba95bd0eb49d8e219f62058b695e1e
yolk-pie-L <493577282@qq.com>
2024-03-17 10:40:28 +0800

    add logging

--- diffusers/sdxl-turbo.py
+++ diffusers/sdxl-turbo.py
@@ -3,6 +3,7 @@ from kserve import Model, ModelServer
 from typing import Dict, Union
 import base64
 from io import BytesIO
+import logging
 
 class ImageModel(Model):
     def __init__(self, name: str):
@@ -15,10 +16,16 @@ class ImageModel(Model):
         self.ready = True
 
     def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
+        # add log about payload
+        logging.info("Start prediction")
+        logging.info(payload)
+
         image = self.pipeline(prompt=payload['instances'][0], num_inference_steps=1, guidance_scale=0.0).images[0]
         buffered = BytesIO()
         image.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
+
+        logging.info("End prediction")
         return {"instances": img_str}
 
 
