ff1dbe976713cf9ca148ff4e7b90b5a30e8d3993
yolk-pie-L <493577282@qq.com>
2024-03-26 11:32:53 +0800

    update preprocess in translator

--- translator/fr-en/model.py
+++ translator/fr-en/model.py
@@ -15,6 +15,23 @@ class Translator(Model):
         self.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-fr-en")
         self.ready = True
 
+    def preprocess(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
+        # Analyze the instances to determine the most frequent instance
+        instances_counter = {}
+
+        for key in payload:
+            instances = tuple(payload[key]["instances"])  # Convert the list to a tuple so it can be used as a dictionary key
+            if instances in instances_counter:
+                instances_counter[instances] += 1
+            else:
+                instances_counter[instances] = 1
+
+        # Find the instance with the highest count
+        most_frequent_instance = max(instances_counter, key=instances_counter.get)
+
+        result = {"instances": list(most_frequent_instance)}
+        return result
+
     def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
         # add log about payload
         logging.info("Start prediction")
